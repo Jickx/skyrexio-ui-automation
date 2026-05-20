3387d40362e6e526c932f2579d452dfbd09faecc
Valentin Sharonov <valentin.sharonov@gmail.com>
2026-05-20 18:12:48 +0300

    Add base framework for Selenium test automation with Allure integration

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class MainLocators:
-    # В интерфейсе текст может меняться, поэтому используем несколько сигналов
     SUCCESS_TITLE = (
         By.XPATH,
         "//h1[contains(., 'Dashboard') or contains(., 'Welcome')]"
